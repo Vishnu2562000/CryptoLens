d3013d5db7e8fc8c3747a8ca5dac36e66555f84d
Vishnu Vardhan Chandragiri <chandragirivishnuvardhan@gmail.com>
2023-04-05 17:56:31 +0530

    Added Mailing functionality

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,13 +1,24 @@
-const primaryBtn = 'nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:py-4 minlg:px-8 font-poppins font-semibold text-white';
-const outlineBtn = 'border border-nft-red-violet bg-transparent font-poppins font-semibold text-sm minlg:text-lg py-2 px-6 minlg:py-4 minlg:px-8';
+const primaryBtn =
+  "nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:py-4 minlg:px-8 font-poppins font-semibold text-white";
+const outlineBtn =
+  "border border-nft-red-violet bg-transparent font-poppins font-semibold text-sm minlg:text-lg py-2 px-6 minlg:py-4 minlg:px-8";
 
-const Button = ({ btnName, classStyles, btnType, handleClick }) => (
+const Button = ({
+  btnName,
+  classStyles,
+  btnType,
+  handleClick,
+  name,
+  handleMailClick,
+}) => (
   <button
     type="button"
     className={
-        btnType === 'primary' ? `${primaryBtn} ${classStyles}` : `${outlineBtn} ${classStyles}`
+      btnType === "primary"
+        ? `${primaryBtn} ${classStyles}`
+        : `${outlineBtn} ${classStyles}`
     }
-    onClick={handleClick}
+    onClick={name ? handleMailClick : handleClick}
   >
     {btnName}
   </button>
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
 import { useTheme } from "next-themes";
+import { useState } from "react";
+import Link from "next/link";
 
 import images from "../assets";
 import Button from "./Button";
+
+const links = [
+  "https://www.instagram.com/vishnuvardhan.chandragiri/",
+  "https://twitter.com/Vishnu2562",
+  "https://t.me/@Conqueror2562",
+  "https://discord.gg/mYxsYFVP",
+];
 
 const FooterLinks = ({ heading, items, extraClasses }) => (
   <div className={`flex-1 justify-start items-start ${extraClasses}`}>
@@ -10,18 +19,35 @@ const FooterLinks = ({ heading, items, extraClasses }) => (
       {heading}
     </h3>
     {items.map((item, index) => (
-      <p
-        key={item + index}
-        className="font-poppins dark:text-white text-nft-black-1 font-normal text-base cursor-pointer dark:hover:text-nft-gray-1 hover:text-nft-black-1 my-3"
+      <Link
+        href={item[1]}
+        target="_blank"
+        rel="noopener noreferrer"
+        key={index}
       >
-        {item}
-      </p>
+        <p
+          key={item[0] + index}
+          className="font-poppins dark:text-white text-nft-black-1 font-normal text-base cursor-pointer dark:hover:text-nft-gray-1 hover:text-nft-black-1 my-3"
+        >
+          {item[0]}
+        </p>
+      </Link>
     ))}
   </div>
 );
 
 const Footer = () => {
   const { theme } = useTheme();
+  const [name, setName] = useState("");
+
+  const handleEmailClick = () => {
+    const mailtoLink = `mailto:chandragirivishnuvardhan@gmail.com?subject=Email from ${name}`;
+    window.open(mailtoLink, "_blank");
+  };
+
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
 
   return (
     <footer className="flexCenter flex-col border-t dark:border-nft-black-1 border-nft-gray-1 sm:py-8 py-16">
@@ -45,7 +71,9 @@ const Footer = () => {
           <div className="flexBetween md:w-full minlg:w-557 w-357 mt-6 dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 rounded-md">
             <input
               type="email"
-              placeholder="Your Email"
+              placeholder="Your Name"
+              value={name}
+              onChange={handleInputChange}
               className="h-full flex-1 w-full dark:bg-nft-black-2 bg-white px-4 rounded-md font-poppins dark:text-white text-nft-black-1 font-normal text-xs minlg:text-lg outline-none"
             />
             <div className="flex-initial">
@@ -53,6 +81,8 @@ const Footer = () => {
                 btnName="Email me"
                 btnType="primary"
                 classStyles="rounded-md"
+                name={name}
+                handleMailClick={() => handleEmailClick(name)}
               />
             </div>
           </div>
@@ -61,15 +91,31 @@ const Footer = () => {
         <div className="flex-1 flexBetweenStart flex-wrap ml-10 md:ml-0 md:mt-8">
           <FooterLinks
             heading="CryptoLens"
-            items={["Explore", "How it Works", "Contact Us"]}
+            items={[
+              ["Explore", "https://github.com/Vishnu2562000/CryptoLens"],
+              [
+                "How it Works",
+                "https://github.com/Vishnu2562000/CryptoLens/blob/main/README.md",
+              ],
+              ["Contact Us", "https://vishnu-vardhan-chandragiri.netlify.app/"],
+            ]}
           />
           <FooterLinks
             heading="Support"
             items={[
-              "Help Center",
-              "Terms of service",
-              "Legal",
-              "Privacy policy",
+              ["Help Center", "https://github.com/Vishnu2562000/CryptoLens"],
+              [
+                "Terms of service",
+                "https://github.com/Vishnu2562000/CryptoLens/blob/main/LICENSE",
+              ],
+              [
+                "Legal",
+                "https://github.com/Vishnu2562000/CryptoLens/blob/main/LICENSE",
+              ],
+              [
+                "Privacy policy",
+                "https://github.com/Vishnu2562000/CryptoLens/blob/main/LICENSE",
+              ],
             ]}
             extraClasses="ml-4"
           />
@@ -88,17 +134,24 @@ const Footer = () => {
               images.telegram,
               images.discord,
             ].map((image, index) => (
-              <div className="mx-2 cursor-pointer" key={`image ${index}`}>
-                <Image
-                  src={image}
-                  key={index}
-                  objectFit="contain"
-                  width={24}
-                  height={24}
-                  alt="social"
-                  className={theme === "light" ? "filter invert" : undefined}
-                />
-              </div>
+              <Link
+                href={links[index]}
+                target="_blank"
+                relative="noreferrer noopener"
+                key={index}
+              >
+                <div className="mx-2 cursor-pointer" key={`image ${index}`}>
+                  <Image
+                    src={image}
+                    key={index}
+                    objectFit="contain"
+                    width={24}
+                    height={24}
+                    alt="social"
+                    className={theme === "light" ? "filter invert" : undefined}
+                  />
+                </div>
+              </Link>
             ))}
           </div>
         </div>
