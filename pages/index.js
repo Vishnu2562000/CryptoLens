bffa00f82266170c4cb768dd8af51493c9118b3f
Vishnu Vardhan Chandragiri <chandragirivishnuvardhan@gmail.com>
2023-04-07 17:04:35 +0530

    added suitable headers

--- pages/index.js
+++ pages/index.js
@@ -131,7 +131,7 @@ const Home = () => {
           <>
             <div>
               <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">
-                Best Creators
+                Top Sellers
               </h1>
 
               <div
@@ -199,7 +199,7 @@ const Home = () => {
             <div className="mt-10">
               <div className="flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
                 <h1 className="flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">
-                  Hot Bids
+                  Hot NFTs
                 </h1>
 
                 <div className="flex-2 sm:w-full flex flex-row sm:flex-col">
